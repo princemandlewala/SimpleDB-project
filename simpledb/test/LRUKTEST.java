5bde44ecb264ae8326073a3b7601f74086c39077
princemandlewala <prince001mandlewala@gmail.com>
2017-12-01 17:41:06 -0500

    Temporary 1

--- simpledb/test/LRUKTEST.java
+++ simpledb/test/LRUKTEST.java
@@ -14,7 +14,7 @@ public class LRUKTEST {
 		SimpleDB.initFileLogAndBufferMgr("lruktest");
 		int numOfBuffers = 4;
 		BufferMgr bufferManager = new BufferMgr(numOfBuffers,2);
-		Block a1 = new Block("A  1", 1);
+		Block a1 = new Block("A1", 1);
 		Block a2 = new Block("A2", 2);
 		Block a3 = new Block("A3", 3);
 		Block a4 = new Block("A4", 4);
